146df20e4320b6f4708270851c092c7f60a8a633
Abdul Al Mamun <mail4mystudies@gmail.com>
2025-07-29 20:09:56 +0600

    changes made

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const mode = document.getElementById(".mode");
 const btn = document.getElementById("btn");
 const text = document.querySelector(".text");
 
-const arr = ["sex", "fuck", "kutta", "gay", "omanush", "dhoincha"];
-
 btn.onclick = function () {
   let number = Math.floor(Math.random() * 6);
   text.classList.remove("hidden");
-  text.textContent = `${arr[number]}`;
+  text.textContent = `You are Gay`;
 };
